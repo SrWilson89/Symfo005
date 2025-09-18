1c96de58f93bc0e1492aae92fc447498de63e774
SrWilson89 <angel481989@gmail.com>
2025-09-18 13:48:27 +0200

    mark 0.2

--- src/Form/EstadosType.php
+++ src/Form/EstadosType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Estados;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class EstadosType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('estado')
+            ->add('estado', TextType::class, [
+                'label' => 'Estado',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Ej. Activo, Pendiente, Finalizado'],
+            ])
         ;
     }
 
